3519dacd8ef113de90a11121564338bb5fb03a0b
Patrik Kulíšek <kulisek@grapesc.cz>
2018-03-06 12:50:05 +0100

    new command for assets - show packages, show limits command - new option -f (filter by limit), -d (show description - hidden by default)

---
+++ commands/AssetsShowLimitsCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Grapesc\GrapeFluid\CoreModule\Command;
+
+use Grapesc\GrapeFluid\AssetRepository;
+use Grapesc\GrapeFluid\BaseParametersRepository;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class AssetsShowLimitsCommand extends Command
+{
+
+	/** @var BaseParametersRepository @inject */
+	public $params;
+
+	/** @var AssetRepository @inject */
+	public $assets;
+
+
+	public function configure()
+	{
+		$this->setName('assets:limits-show')
+			->addOption("copy", "c", InputOption::VALUE_NONE, "Include copy files")
+			->addOption("filter", "f", InputOption::VALUE_OPTIONAL, "Filter by given Nette link", null)
+			->addOption("description", "d", InputOption::VALUE_NONE, "Show file description");
+	}
+
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		if (($filter = $input->getOption("filter")) !== null) {
+			$output->writeln("Assets only for limit [$filter]:");
+			$this->processFiles($this->assets->getForCurrentLink("css", $filter) + $this->assets->getForCurrentLink("js", $filter), $input, $output);
+		} else {
+			foreach ($this->assets->getFileList() as $limit => $files) {
+				if (empty($files)) {
+					continue;
+				}
+				$output->writeln("<info>Assets for limit [$limit]:</info>");
+				$this->processFiles($files, $input, $output);
+				$output->write("\n");
+			}
+		}
+	}
+
+
+	private function processFiles($files, InputInterface $input, OutputInterface $output)
+	{
+		foreach ($files as $path => $desc) {
+			if ($input->getOption("copy") === false && $desc['type'] == "copy") {
+				continue;
+			} else {
+				$output->writeln("<info>" . str_replace(DIRECTORY_SEPARATOR, "/", str_replace($this->params->getParam("appDir"), "", $path)) . ($input->getOption("description") ? " - " . json_encode($desc) : "") . "</info>");
+			}
+		}
+	}
+
+}
---
+++ commands/AssetsShowPackagesCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Grapesc\GrapeFluid\CoreModule\Command;
+
+use Grapesc\GrapeFluid\AssetRepository;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class AssetsShowPackagesCommand extends Command
+{
+
+	/** @var AssetRepository @inject */
+	public $assets;
+
+
+	public function configure()
+	{
+		$this->setName('assets:packages-show');
+	}
+
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$output->writeln("<info>Assets (sorted, removed disabled):</info>");
+		foreach ($this->assets->getAssets() as $name => $asset) {
+			$output->writeln($name);
+		}
+	}
+
+}
